44426b3d750d20ef84eee8e0d43dab9f906b6d9b
weiguowang <guowang1102@163.com>
2017-03-08 00:08:34 +0800

    toasty

--- app/src/main/java/com/weiguowang/schoolmate/SchoolApp.java
+++ app/src/main/java/com/weiguowang/schoolmate/SchoolApp.java
@@ -9,7 +9,7 @@ import cn.bmob.v3.Bmob;
 /**
  * function:
  * Created by 韦国旺 on 2017/3/7 0007.
- * Copyright (c) 2017 北京联龙博通 All Rights Reserved.
+ * Copyright (c) 2017 All Rights Reserved.
  */
 public class SchoolApp extends Application {
     @Override
