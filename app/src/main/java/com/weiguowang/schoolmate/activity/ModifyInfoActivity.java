fc887c3e87b981f9874065c5a813f14b9125f4d7
wells <guowang1102@163.com>
2017-03-13 16:19:59 +0800

    新增我的信息界面处理

--- app/src/main/java/com/weiguowang/schoolmate/activity/ModifyInfoActivity.java
+++ app/src/main/java/com/weiguowang/schoolmate/activity/ModifyInfoActivity.java
@@ -3,10 +3,11 @@ package com.weiguowang.schoolmate.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.weiguowang.schoolmate.R;
 import com.weiguowang.schoolmate.TActivity;
 
 /**
- * function:
+ * function: 修改信息
  * Created by 韦国旺 on 2017/3/9 0009.
  * Copyright (c) 2017  All Rights Reserved.
  */
@@ -15,5 +16,6 @@ public class ModifyInfoActivity extends TActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_myinfo);
     }
 }
--- app/src/main/java/com/weiguowang/schoolmate/activity/MyInfoActivity.java
+++ app/src/main/java/com/weiguowang/schoolmate/activity/MyInfoActivity.java
@@ -1,9 +1,12 @@
 package com.weiguowang.schoolmate.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.weiguowang.schoolmate.R;
 import com.weiguowang.schoolmate.TActivity;
@@ -15,23 +18,49 @@ import com.weiguowang.schoolmate.TActivity;
  */
 public class MyInfoActivity extends TActivity {
 
+    private TextView modifyTv;
+
+    public static final int CODE_BACK_MODIFY = 501;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myinfo);
+        initView();
+
+    }
+
+    private void initView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("我的信息");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        modifyTv = (TextView) findViewById(R.id.modify_tv);
+        modifyTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyInfoActivity.this, ModifyInfoActivity.class);
+                startActivityForResult(intent, CODE_BACK_MODIFY);
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == CODE_BACK_MODIFY) {
+            //TODO 刷新数据
+
+        }
     }
 }
