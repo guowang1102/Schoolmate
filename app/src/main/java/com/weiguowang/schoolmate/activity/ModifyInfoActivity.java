9d3a23b9f53a4ddd8966b3f3d8af3b26bc6f3bcc
wells <guowang1102@163.com>
2017-03-17 11:04:42 +0800

    UserInfo modify

--- app/src/main/java/com/weiguowang/schoolmate/activity/ModifyInfoActivity.java
+++ app/src/main/java/com/weiguowang/schoolmate/activity/ModifyInfoActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.weiguowang.schoolmate.R;
 import com.weiguowang.schoolmate.TActivity;
@@ -15,7 +14,6 @@ import com.weiguowang.schoolmate.entity.School;
 import com.weiguowang.schoolmate.view.ListPopupWindow;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import cn.bmob.v3.BmobQuery;
@@ -27,17 +25,13 @@ import cn.bmob.v3.listener.UpdateListener;
 /**
  * function: 修改信息
  * Created by 韦国旺 on 2017/3/9.
- * Copyright (c) 2017  All Rights Reserved.
+ * Copyright (c) 2017 All Rights Reserved.
  */
 public class ModifyInfoActivity extends TActivity implements View.OnClickListener {
 
-    private EditText nickNameEt, realNameEt, sexEt, jobEt, mobilePhoneEt, collegeEt, majorEt, sessionEt;
-
-    private TextView schoolNameTv;
-
+    private EditText nickNameEt, realNameEt, sexEt, jobEt, mobilePhoneEt, schoolNameeEt, collegeEt, majorEt, sessionEt;
     private MyUser userInfo;
-
-    private ArrayList<String> arrayList = new ArrayList<>();
+    private ArrayList<String> schoolNameList = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,7 +53,7 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
         sexEt = (EditText) findViewById(R.id.sex);
         jobEt = (EditText) findViewById(R.id.job);
         mobilePhoneEt = (EditText) findViewById(R.id.mobile_phone);
-        schoolNameTv = (TextView) findViewById(R.id.school_name);
+        schoolNameeEt = (EditText) findViewById(R.id.school_name);
         collegeEt = (EditText) findViewById(R.id.college);
         majorEt = (EditText) findViewById(R.id.major);
         sessionEt = (EditText) findViewById(R.id.session);
@@ -68,11 +62,10 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
     private void initData() {
         userInfo = BmobUser.getCurrentUser(MyUser.class);
         setUserInfo(userInfo);
-//        arrayList.add("广西建设职业技术学院");
-//        arrayList.add("广西财经学院");
     }
 
     private void initEvent() {
+
     }
 
     /**
@@ -84,7 +77,7 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
         sexEt.setText(myUser.getSex() ? getString(R.string.female) : getString(R.string.male));
         jobEt.setText(myUser.getJob());
         mobilePhoneEt.setText(myUser.getMobilePhoneNumber());
-        schoolNameTv.setText(myUser.getSchoolName());
+        schoolNameeEt.setText(myUser.getSchoolName());
         collegeEt.setText(myUser.getCollege());
         majorEt.setText(myUser.getMajor());
         sessionEt.setText(myUser.getSession());
@@ -109,7 +102,7 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
         myUser.setRealName(realNameEt.getText().toString());
         myUser.setSex(false);  //TODO
         myUser.setJob(jobEt.getText().toString());
-        myUser.setSchoolName(schoolNameTv.getText().toString());
+        myUser.setSchoolName(schoolNameeEt.getText().toString());
         myUser.setCollege(collegeEt.getText().toString());
         myUser.setMajor(majorEt.getText().toString());
         myUser.setSession(sessionEt.getText().toString());
@@ -135,41 +128,140 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
                 updateUserInfo();
                 break;
             case R.id.modify_school_name:
-                getSchoolList();
-//                chooseSchoolName();
+                getSchoolNameList();
+                break;
+            case R.id.modify_college:
+                getCollegeList();
+                break;
+            case R.id.modify_major:
+                getMajorList();
+                break;
+            case R.id.modify_session:
+                getSessionList();
                 break;
         }
     }
 
-    public void getSchoolList(){
-        arrayList.clear();
+    public void getSchoolNameList() {
+        schoolNameList.clear();
         BmobQuery<School> query = new BmobQuery<>();
         query.order("-createdAt").findObjects(new FindListener<School>() {
             @Override
             public void done(List<School> list, BmobException e) {
-                if(list.size()>0){
-                    for(School school: list){
-                        arrayList.add(school.getSchoolName());
+                if (list.size() > 0) {
+                    for (School school : list) {
+                        schoolNameList.add(school.getSchoolName());
                     }
-                   chooseSchoolName();
+                    chooseList(schoolNameList, new ListPopupWindow.OnSelectListener() {
+                        @Override
+                        public void getValue(String value) {
+                            toastyInfo(value);
+                            schoolNameeEt.setText(value);
+                        }
+                    }, findViewById(R.id.modify_school_name));
                 }
             }
         });
     }
 
+    /**
+     *
+     */
+    public void getCollegeList() {
+        BmobQuery<School> query = new BmobQuery<>();
+        final String schoolName = schoolNameeEt.getText().toString().trim();
+        query.addWhereEqualTo("schoolName", schoolName);
+        query.setLimit(100);
+        query.findObjects(new FindListener<School>() {
+            @Override
+            public void done(List<School> schoolList, BmobException e) {
+                ArrayList<String> collegeList = new ArrayList<>();
+                if (schoolList.size() > 0) {
+                    for (School school : schoolList) {
+                        collegeList.add(school.getCollege());
+                    }
+                    chooseList(collegeList, new ListPopupWindow.OnSelectListener() {
+                        @Override
+                        public void getValue(String value) {
+                            toastyInfo(value);
+                            collegeEt.setText(value);
+                        }
+                    }, findViewById(R.id.modify_college));
+                }
+
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    private void getMajorList() {
+        BmobQuery<School> query = new BmobQuery<>();
+        final String schoolName = schoolNameeEt.getText().toString().trim();
+        final String college = collegeEt.getText().toString().trim();
+        query.addWhereEqualTo("schoolName", schoolName);
+        query.addWhereEqualTo("college", college);
+        query.setLimit(100);
+        query.findObjects(new FindListener<School>() {
+            @Override
+            public void done(List<School> schoolList, BmobException e) {
+                ArrayList<String> majorList = new ArrayList<>();
+                if (schoolList.size() > 0) {
+                    for (School school : schoolList) {
+                        majorList.add(school.getMajor());
+                    }
+                    chooseList(majorList, new ListPopupWindow.OnSelectListener() {
+                        @Override
+                        public void getValue(String value) {
+                            toastyInfo(value);
+                            majorEt.setText(value);
+                        }
+                    }, findViewById(R.id.modify_major));
+                }
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    private void getSessionList() {
+        BmobQuery<School> query = new BmobQuery<>();
+        final String schoolName = schoolNameeEt.getText().toString().trim();
+        final String college = collegeEt.getText().toString().trim();
+        final String major = majorEt.getText().toString().trim();
+        query.addWhereEqualTo("schoolName", schoolName);
+        query.addWhereEqualTo("college", college);
+        query.addWhereEqualTo("major", major);
+        query.setLimit(100);
+        query.findObjects(new FindListener<School>() {
+            @Override
+            public void done(List<School> schoolList, BmobException e) {
+                final ArrayList<String> sessionList = new ArrayList<>();
+                if (schoolList.size() > 0) {
+                    for (School school : schoolList) {
+                        sessionList.add(school.getSession());
+                    }
+                    chooseList(sessionList, new ListPopupWindow.OnSelectListener() {
+                        @Override
+                        public void getValue(String value) {
+                            toastyInfo(value);
+                            sessionEt.setText(value);
+                        }
+                    }, findViewById(R.id.modify_session));
+                }
+            }
+        });
+    }
+
+
     private ListPopupWindow popupWindow;
 
-    private void chooseSchoolName() {
+    private void chooseList(ArrayList<String> arrayList, ListPopupWindow.OnSelectListener listener, View AsDropDownView) {
         if (popupWindow == null) {
             popupWindow = new ListPopupWindow(getApplicationContext(), arrayList);
-//            popupWindow.setMenuViewOnSelectListener(new NMCascadingMenuViewOnSelectListener());
-            popupWindow.setSelectListener(new ListPopupWindow.OnSelectListener() {
-                @Override
-                public void getValue(String value) {
-                    toastyInfo(value);
-                    schoolNameTv.setText(value);
-                }
-            });
+            popupWindow.setSelectListener(listener);
             popupWindow.showAsDropDown(findViewById(R.id.modify_school_name), 5, 5);
         } else if (popupWindow != null && popupWindow.isShowing()) {
             popupWindow.dismiss();
@@ -178,4 +270,14 @@ public class ModifyInfoActivity extends TActivity implements View.OnClickListene
         }
 
     }
+
+    public interface QueryDoneListener {
+        /**
+         * @param obj
+         * @param e
+         */
+        void done(Object obj, BmobException e);
+    }
+
+
 }
